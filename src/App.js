6d804c342831691e85845aeaaeed922537f6ad2b
Lim Kimhuor <kimhuorlim@gmail.com>
2018-10-21 05:56:32 +0700

    react context api 16.3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const MyContext = React.createContext();
+
+class MyProvider extends Component {
+  state = {
+    name: "Kim",
+    age: 27,
+    cool: true
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          increaseAge: () =>
+            this.setState({
+              age: this.state.age + 1
+            })
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+const Family = props => (
+  <div className="family">
+    <Person />
+  </div>
+);
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {context => (
+            <Fragment>
+              <p>Name: {context.state.name}</p>
+              <p>Age: {context.state.age}</p>
+              <button onClick={context.increaseAge}>Increase Age</button>
+            </Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MyProvider>
+        <div className="App">
+          <p>I'm new to react.</p>
+          <Family />
+        </div>
+      </MyProvider>
     );
   }
 }
